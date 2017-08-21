e588e27f6273ac5a36947873da4149d6e9aa8e12
加藤公彦 <kimihiko@katoukimihiko-no-MacBook-Pro.local>
2017-08-21 21:39:29 +0900

    small fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  has_many :blogs
   has_many :blogs, primary_key: name, foreign_key: name
 end
